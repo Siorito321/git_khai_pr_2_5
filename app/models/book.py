bc4a6d8f7865c95d34b56c8fb6e67e872c3e3b51
Nikita-pyth <siorito321@gmail.com>
2025-05-10 12:50:24 +0300

    book model updated according to the constructor injection pattern

--- app/models/book.py
+++ app/models/book.py
@@ -1,11 +1,12 @@
 from app.interfaces.book_interface import BookInterface
 
-
 class Book(BookInterface):
-    def __init__(self, title: str, author: str, year: int):
-        self.title = title
-        self.author = author
-        self.year = year
+    """Dependent class"""
+    def __init__(self, title_provider, author_provider, year_provider):
+
+        self.title = title_provider.get_title()
+        self.author = author_provider.get_author()
+        self.year = year_provider.get_publication_year()
         self.available = True
 
     def get_title(self) -> str:
@@ -22,3 +23,28 @@ class Book(BookInterface):
 
     def set_availability(self, available: bool) -> None:
         self.available = available
+
+
+class TitleProvider:
+    """Independent class"""
+    def __init__(self, title: str):
+        self._title = title
+
+    def get_title(self) -> str:
+        return self._title
+
+class AuthorProvider:
+    """Independent class"""
+    def __init__(self, author: str):
+        self._author = author
+
+    def get_author(self) -> str:
+        return self._author
+
+class YearProvider:
+    """Independent class"""
+    def __init__(self, year: int):
+        self._year = year
+
+    def get_publication_year(self) -> int:
+        return self._year
